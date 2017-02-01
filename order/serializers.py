fbd65f32168391e26ab6c49f6d6b5b00bedbf0af
Sean B. Powell <sean@bendtsenpowell.com>
2017-02-01 16:34:34 +0100

    Added rest api for creating orders
    Post something at /api/orders/. The product is found by name

--- order/serializers.py
+++ order/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from order import models
+from django.db import transaction
+from product.models import Product
+from rhinorun.consumers import send_all
+
 
 class OrderItemSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source='product_name')
+
     class Meta:
         model = models.OrderItem
         fields = ('id', 'quantity', 'name')
@@ -14,3 +19,22 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Order
         fields = ('id', 'state', 'user', 'items', 'total')
+
+    def create(self, validated_data):
+
+        items = self.initial_data.pop('items', None)
+
+        with transaction.atomic():
+            obj = super().create(validated_data)
+
+            for item in items:
+                name = item.pop('name')
+                quantity = item.pop('quantity')
+
+                # Hah, such programming.
+                prod = Product.objects.get(name__iexact=name)
+                obj.items.create(product=prod, quantity=quantity)
+
+        send_all()
+
+        return obj
