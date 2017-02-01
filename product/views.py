0dc369acec83ec4e4f587747832d3a68f20ede1f
Kyle Thomson <kjay.thomson@gmail.com>
2017-02-01 21:17:06 +0100

    product list

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Product
 
-# Create your views here.
+class ProductListView(View):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+        	'products': Product.objects.all()
+        }
+
+        return render(request, self.template_name, context)
